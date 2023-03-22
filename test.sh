72d3d956081df11f39ec0225afb563a5fedba831
lcwLcw123 <lcw1264942837@gami.com>
2023-03-23 02:54:40 +0800

    2023.3.23

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
-mkdir result/
-mkdir test_data/
-mkdir saved_models/
 python IS-Net/Inference.py
 python inference_final.py -opt1 options/test/BOKEN/NAFNet-width64_125_debokeh.yml -opt2 options/test/BOKEN/NAFNet-width64_125_bokeh.yml
